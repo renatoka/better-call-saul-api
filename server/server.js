83fb4eb2286fd2c7cbab66f8473a20a1edbe1a24
Renato <rkauric@vvg.hr>
2022-11-08 18:29:12 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
