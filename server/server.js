9ac2e54d777dfc2240ae97d9cd88d8cfc1a3a6bd
Renato <rkauric@vvg.hr>
2022-11-08 19:11:02 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+let port = process.env.PORT || 3000;
 require('dotenv').config();
 const app = express();
 
@@ -9,7 +10,9 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB Database'));
 
-app.listen(process.env.PORT || 3001, '0.0.0.0' , () => console.log(`Server started on port ${PORT}`));
+app.listen(port, '0.0.0.0', () => {
+    console.log(`app running on ${port} `);
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
