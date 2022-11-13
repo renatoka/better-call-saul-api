b81ab83767362015afd919d8a2a18d393cf573a1
Renato <rkauric@vvg.hr>
2022-11-13 15:04:52 +0100

    Added query search by title, season, directed-by and written-by

--- server/routes/episodes.js
+++ server/routes/episodes.js
@@ -2,19 +2,53 @@ const express = require('express');
 const router = express.Router();
 const Episode = require('../models/episode');
 
-// Get All Episodes
-router.get('/', async (req, res, next) => {
+/*
+Get All Episodes
+13.11.2022.
+-----------
+Added Query search by title and season
+*/
+
+router.get('/', async (req, res) => {
+    const { title, season, directed_by, written_by } = req.query;
     try {
-        const episodes = await Episode.find();
-        if (episodes.length == 0) return res.status(404).json({ message: 'Database Error. No episodes found. Try again later.' });
-        else { res.status(200).json(episodes) };
+        if (title && season && directed_by && written_by) {
+            const episodes = await Episode.find({ title, season });
+            if (episodes.length == 0) return res.status(404).json({ message: 'No episode with that title and season found.' });
+            else return res.status(200).json(episodes);
+        }
+        else if (title) {
+            const episodes = await Episode.find({ title });
+            if (episodes.length == 0) return res.status(404).json({ message: 'No episode with that title found.' });
+            else return res.status(200).json(episodes);
+        }
+        else if (season) {
+            const episodes = await Episode.find({ season });
+            if (episodes.length == 0) return res.status(404).json({ message: 'No episode with that season found.' });
+            else return res.status(200).json(episodes);
+        }
+        else if (directed_by) {
+            const episodes = await Episode.find({ directed_by });
+            if (episodes.length == 0) return res.status(404).json({ message: 'No episode with that director found.' });
+            else return res.status(200).json(episodes);
+        }
+        else if (written_by) {
+            const episodes = await Episode.find({ written_by });
+            if (episodes.length == 0) return res.status(404).json({ message: 'No episode with that writer found.' });
+            else return res.status(200).json(episodes);
+        }
+        else {
+            const episodes = await Episode.find();
+            if (episodes.length == 0) return res.status(404).json({ message: 'Database Error. No episodes found. Try again later.' });
+            else return res.status(200).json(episodes);
+        }
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        return res.status(500).json({ message: err.message });
     }
 });
 
 // Get Random Episode
-router.get('/random', async (req, res, next) => {
+router.get('/random', async (req, res) => {
     try {
         const episodes = await Episode.find();
         if (episodes.length == 0) return res.status(404).json({ message: 'Database Error. No episodes found. Try again later.' });
@@ -27,19 +61,8 @@ router.get('/random', async (req, res, next) => {
     }
 });
 
-// Get Episode by Title
-router.get('/episode/:title', async (req, res, next) => {
-    try {
-        const episode = await Episode.findOne({ title: req.params.title });
-        if (episode == null) return res.status(404).json({ message: 'Episode doesn\'t exist. Check spelling and try again.' });
-        else { res.status(200).json(episode) };
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
 // Get Specific Episode by ID
-router.get('/:episode_id', async (req, res, next) => {
+router.get('/:episode_id', async (req, res) => {
     try {
         const episode = await Episode.find({ episode_id: req.params.episode_id });
         if (episode.length == 0) return res.status(404).json({ message: 'Episode doesn\'t exist. Check spelling and try again.' });
@@ -50,7 +73,7 @@ router.get('/:episode_id', async (req, res, next) => {
 });
 
 // Create an Episode
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
     const episode = new Episode({
         episode_id: req.body.episode_id,
         title: req.body.title,
