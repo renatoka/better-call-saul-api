bdfd9511c05cab5fb285ecbaaba1b052c56cc2a5
Renato <rkauric@vvg.hr>
2023-06-23 14:05:27 +0200

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CharactersModule } from './characters/characters.module';
+import { QuotesModule } from './quotes/quotes.module';
+import { EpisodesModule } from './episodes/episodes.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Breaking Bad API')
+    .setDescription('The Breaking Bad API swagger documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [CharactersModule, QuotesModule, EpisodesModule],
+  });
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
