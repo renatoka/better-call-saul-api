c605540fd0893e1d6581ebcffc201b8330bd1daa
Renato <rkauric@vvg.hr>
2023-06-23 14:06:37 +0200

    Implement basic characters functionality

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CharactersService } from './characters.service';
+import { ApiProperty, ApiResponse } from '@nestjs/swagger';
 
 @Controller('api/characters')
 export class CharactersController {
   constructor(private readonly charactersService: CharactersService) {}
 
   @Get()
+  @ApiProperty({
+    description: 'Get all characters',
+    required: false,
+  })
   async findAll() {
-    await this.charactersService.findAll();
+    return await this.charactersService.findAll();
   }
 
+  @ApiProperty({
+    description: 'Get all characters by ID',
+    required: false,
+  })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.charactersService.findOne(+id);
